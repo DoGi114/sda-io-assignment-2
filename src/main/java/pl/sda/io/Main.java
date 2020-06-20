4e16abef526095db7f828131ecac5e0e434cb4d4
Damian Nguyen <damian.nguyen93@gmail.com>
2020-06-20 13:09:17 +0200

    Task 1

--- src/main/java/pl/sda/io/Main.java
+++ src/main/java/pl/sda/io/Main.java
@@ -1,4 +1,15 @@
 package pl.sda.io;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
         /*
@@ -7,5 +18,24 @@ public class Main {
         2. Poza wypisaniem nazw plików wypisz też ilość wystąpień tego słowa w danym pliku w formacie <nazwa>:<ilość wystąpień>
         3. Zamiast wypisywać na konsolę zapis rezultat do pliku
          */
+        findGiveWordInFiles("coś");
+    }
+
+    private static void findGiveWordInFiles(String givenWord){
+        try {
+            List<Path> filePaths = Files.list(Paths.get("my-files")).collect(Collectors.toList());
+            for(Path path : filePaths){
+                BufferedReader bufferedReader = Files.newBufferedReader(path);
+                String content;
+                while( ( content = bufferedReader.readLine() ) != null){
+                    if(content.contains(givenWord)){
+                        System.out.println(path);
+                        break;
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
